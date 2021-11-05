0661a0fddf3ba4adb2b06fbd7f143ee396dbdd23
Kuntal Das <25ckuntaldas@gmail.com>
2021-11-05 18:47:30 +0530

    basic GET dnamic GET

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using System.Collections.Generic;
+using Catalog.Entities;
+using System;
+using System.Linq;
+using Catalog.Dtos;
+
+namespace Catalog.Controllers
+{
+    // Get /items
+    [ApiController]
+    [Route("items")]
+    // [Route("[contoller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // Get /items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDto());
+            return items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item.AsDto();
+        }
+    }
+}
